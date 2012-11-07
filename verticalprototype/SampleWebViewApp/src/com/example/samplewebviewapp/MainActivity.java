bb6fb6198242a55656b807390cdbea92d2893ab5
sbong <sbong@calpoly.edu>
2012-11-07 14:09:39 -0800

    add webview wrapper

---
+++ verticalprototype/SampleWebViewApp/src/com/example/samplewebviewapp/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.samplewebviewapp;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+	private WebView webView;
+    @SuppressLint("SetJavaScriptEnabled")
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+		super.onCreate(savedInstanceState);
+ 
+		webView = (WebView) findViewById(R.id.webView1);
+		webView.getSettings().setJavaScriptEnabled(true);
+		webView.setWebViewClient(new OverrideWebViewClient());
+		webView.loadUrl("http://kotaku.com/");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+    
+    private class OverrideWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+}
