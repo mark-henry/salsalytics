b2152b5819178fde4019b35ca86ce0f470ced660
Brandon Page <brpage@calpoly.edu>
2012-11-15 17:53:14 -0800

    Added the EventSender class to the sample app project and the necessary jars for the EventSender.

--- verticalprototype/SampleApp/src/com/salsaforce/sampleapp/MainActivity.java
+++ verticalprototype/SampleApp/src/com/salsaforce/sampleapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.salsaforce.sampleapp;
 
+//import com.salsalytics.apiwrapper.EventSender;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
@@ -7,8 +8,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
-import com.salsalytics.*;
-import com.salsalytics.apiwrapper.EventSender;
+//import com.salsalytics.*;
 
 public class MainActivity extends Activity {
 
