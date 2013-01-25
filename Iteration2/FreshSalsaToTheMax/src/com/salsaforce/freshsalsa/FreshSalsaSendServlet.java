20623385a6485cb4f2f61d6fb45175e85dbbeae3
sbong <sbong@calpoly.edu>
2013-01-24 19:59:44 -0800

    gets stuff from the database

--- Iteration2/FreshSalsaToTheMax/src/com/salsaforce/freshsalsa/FreshSalsaSendServlet.java
+++ Iteration2/FreshSalsaToTheMax/src/com/salsaforce/freshsalsa/FreshSalsaSendServlet.java
@@ -1,11 +1,12 @@
 package com.salsaforce.freshsalsa;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
 import javax.servlet.http.*;
 
-import com.google.appengine.api.datastore.DatastoreService;
-import com.google.appengine.api.datastore.DatastoreServiceFactory;
-import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.*;
 
 @SuppressWarnings("serial")
 public class FreshSalsaSendServlet extends HttpServlet {
@@ -15,10 +16,31 @@ public class FreshSalsaSendServlet extends HttpServlet {
 		resp.getWriter().println("This page sends the data in the datastore to salesforce.");
 		
 		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+		Transaction txn = datastore.beginTransaction();
 		
+		try {
+			Query q = new Query("Event");
+			List<Entity> results = 
+				datastore.prepare(q).asList(FetchOptions.Builder.withDefaults());
+			for (Entity curr : results)
+			{
+				Map<String,Object> props = curr.getProperties();
+				for (String key : props.keySet())
+				{
+					resp.getWriter().println(key + " " + props.get(key));
+				}
+			}
+			txn.commit();
+		}
+		finally {
+			if (txn.isActive()) {
+				txn.rollback();
+			}
+		}
 		
 		String title = "";
 		Entity event = new Entity("Event");
+		
 		
 		for (Object key : req.getParameterMap().keySet()) {
 			if (key instanceof String) {
@@ -42,17 +64,4 @@ public class FreshSalsaSendServlet extends HttpServlet {
 		
 	}
 	
-	/*if (sKey.equals("ROO"))
-	{
-		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-		Entity employee = new Entity("Employee","Cory'sKey");
-
-		employee.setProperty("firstName", "Antonio");
-		employee.setProperty("lastName", "Salieri");
-
-		employee.setProperty("attendedHrTraining", true);
-
-
-		datastore.put(employee);
-	}*/
 }
