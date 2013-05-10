cc5dd7f158a2bb26f28dd244b7b41185aac50c84
brpage <brpage@calpoly.edu>
2013-05-10 13:21:00 -0700

    Fixed buges causing crashes/failures on null title and null app name. Moved all EventSender code to a new folder on bitbucket.

---
+++ EventSender/EpicLlamaFarmer/src/com/salsaforce/happyllamafarmer2/EpicLlamaFarmer.java
@@ -0,0 +1,139 @@
+package com.salsaforce.happyllamafarmer2;
+import java.util.TreeMap;
+
+import com.salsaforce.happyllamafarmer2.R;
+
+import salsalytics.EventSender;
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.EditText;
+import android.widget.Switch;
+
+
+public class EpicLlamaFarmer extends Activity {
+	TreeMap<String, String> attributesMap = new TreeMap<String, String>();
+	TreeMap<String, String> constantData = new TreeMap<String, String>();
+	Button sendButton, setAppNameButton;
+	EditText appName, key1, value1, key2, value2, key3, value3;
+	CheckBox cBox;
+	Switch nameSwitch, maufactureSwitch, carrierSwitch;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+				
+		setContentView(R.layout.new_features_layout);
+		
+		EventSender.setURL("http://freshsalsaforce.appspot.com/ReceiveEvents");
+
+		
+		sendButton = (Button) findViewById(R.id.sendButton);
+		setAppNameButton = (Button) findViewById(R.id.setAppNameButton);
+		appName = (EditText) findViewById(R.id.appName);
+		key1 = (EditText) findViewById(R.id.key1);
+		key2 = (EditText) findViewById(R.id.key2);
+		key3 = (EditText) findViewById(R.id.key3);
+		value1 = (EditText) findViewById(R.id.value1);
+		value2 = (EditText) findViewById(R.id.value2);
+		value3 = (EditText) findViewById(R.id.value3);
+		cBox = (CheckBox) findViewById(R.id.checkBox1);
+		nameSwitch = (Switch) findViewById(R.id.deviceNameSwitch);
+		maufactureSwitch = (Switch) findViewById(R.id.manufactureSwitch);
+		carrierSwitch = (Switch) findViewById(R.id.CarrierSwitch);
+		
+		constantData.put("BuildStage", "Beta");
+		constantData.put("BestManagerEver", "Gene Rivera");
+		
+		sendButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				String sKey1 = key1.getText().toString();
+				String sValue1 = value1.getText().toString();
+				String sKey2 = key2.getText().toString();
+				String sValue2= value2.getText().toString();
+				String sKey3 = key3.getText().toString();
+				String sValue3 = value3.getText().toString();
+				
+				if(!sKey1.equals("") && !sValue1.equals(""))
+					attributesMap.put(sKey1, sValue1);
+				if(!sKey2.equals("") && !sValue2.equals(""))
+					attributesMap.put(sKey2, sValue2);
+				if(!sKey3.equals("") && !sValue3.equals(""))
+					attributesMap.put(sKey3, sValue3);
+				
+				EventSender.sendData("EpicDemo", attributesMap);
+			}
+		});
+		
+		setAppNameButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				EventSender.setAppName(appName.getText().toString());
+			}
+		});
+		
+		cBox.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView,
+					boolean isChecked) {
+
+				if(isChecked) 
+					EventSender.setConstantData(constantData);
+				else
+					EventSender.setConstantData(null);
+			}
+		});
+		
+		nameSwitch.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView,
+					boolean isChecked) {
+			
+				EventSender.changeDeviceInformationCollected().setDeviceNameCollected(isChecked);
+			}
+		});
+		
+		this.maufactureSwitch.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView,
+					boolean isChecked) {
+			
+				EventSender.changeDeviceInformationCollected().setManufactureCollected(isChecked);
+			}
+		});
+		
+		this.carrierSwitch.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView,
+					boolean isChecked) {
+			
+				EventSender.changeDeviceInformationCollected().setWirelessServiceProviderCollected(isChecked);
+			}
+		});
+	}	
+	
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	public static Context getContext() {
+		return getContext();
+	}
+}
+
---
+++ EventSender/Salsalytics/src/salsalytics/Event.java
@@ -0,0 +1,153 @@
+package salsalytics;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import android.util.Log;
+import android.widget.Toast;
+
+/**
+ * Event represents a Salesforce Event Object, which live in a 
+ * Salesforce.com database.  An event is all of the data related 
+ * to a single snapshot in time.  The Event contains a title that 
+ * describes what it represents and sets of data in the form of
+ * key-value pairs.  
+ * 
+ * 
+ * @author Brandon Page, brpage@calpoly.edu
+ * @author Martin Silverio, msilverio324@gmail.com
+ */
+public class Event {
+	private String charset = "UTF-8";
+	private String query = "?SalsalyticsEventTitle=";
+	private String url, appName;
+	private Map<String, String> constantData;
+	private Map<String, String> deviceInfo;
+	
+	protected Event(String url, String appName, Map<String, String> constantData, Map<String, String> deviceInformation) {
+		this.url = url;
+		this.appName = appName;
+		this.constantData = constantData;
+		this.deviceInfo = deviceInformation;
+	}
+	
+	/**
+	 * Sends data to the users Google App Engine site.
+	 * 
+	 * @return the return status of sending the data.
+	 */
+	int send() throws MalformedURLException, IOException {
+		HttpURLConnection conn = (HttpURLConnection) new URL(url + query)
+	     .openConnection();
+		conn.setRequestProperty("Accept-Charset", charset);
+		@SuppressWarnings("unused")
+		InputStream response = new BufferedInputStream(conn.getInputStream());
+		int status = conn.getResponseCode();
+		query = "?SalsalyticsEventTitle=";
+
+		return status;
+	}
+
+	/**
+	 * Returns the current server URL. 
+	 * 
+	 * @return the URL of the server
+	 */
+	public URL getServer() {
+		URL realUrl;
+		try {
+			realUrl = new URL(url);
+			return realUrl;
+		} catch (MalformedURLException e) {
+			Log.e("Malformed String", e.getMessage());
+		}
+
+		return null;
+	}
+
+	/**
+	 * Adds a title and event attributes to a querystring to 
+	 * be send to the Server.   
+	 * 
+	 * @param title a String representing the title of the event
+	 * @param attributes a Map containing the key-value pairs 
+	 * relating to the event.
+	 */
+	protected void addData(String title, Map<String, String> attributes) {
+		
+		if(title == null || title.equals("")) {
+    		title = "Unnamed Event";
+    	}
+		
+		if(appName == null || appName.equals(""))
+			appName = "all";
+
+		query += title;
+		query += "&" + "AppName=" + appName;
+		
+		Log.i("queryIncomplete", query);
+		if(this.deviceInfo != null && !this.deviceInfo.isEmpty())
+			query += buildQueryString(this.deviceInfo, false);
+		
+		if(this.constantData != null && !this.constantData.isEmpty()) 
+			query += buildQueryString(this.constantData, false);
+		
+		if(attributes != null && !attributes.isEmpty())
+			query += buildQueryString(attributes, true);
+	}
+
+	/**
+	 * Builds the querystring from a map of key-value pairs.
+	 * 
+	 * @param urlParams Map of key value pairs to be put into they 
+	 * querystring.
+	 * @return The querystring with url encoded parameters.
+	 */
+	private String buildQueryString(Map<String, String> urlParams, boolean checkForSecialChars) {
+		StringBuilder query = new StringBuilder();
+		boolean skipEntry = false;
+
+		for (Entry<String, String> urlParam : urlParams.entrySet()) {
+			
+			skipEntry = (checkForSecialChars && urlParam.getKey().startsWith("$"));
+			if(!skipEntry) {
+				query.append("&" + encode(urlParam.getKey()) + "="
+					+ encode(urlParam.getValue()));
+			}
+		}
+		return query.toString();
+	}
+
+	/**
+	 * Wrapper function for url encoding things for the querystring.
+	 * 
+	 * @param urlParam A parameter to encode.
+	 * @return The encoded parameter or an empty string on error.
+	 */
+	private String encode(String urlParam) {
+		String charset = "UTF-8";
+		try {
+			return URLEncoder.encode(urlParam, charset);
+		} catch (UnsupportedEncodingException exc) {
+			Log.d("Unsupported Encoding Operation", exc.getMessage());
+			return "";
+		}
+	}
+	
+	/**
+	 * A getter for the query string for testing purposes
+	 * 
+	 * @return the queryString currently built
+	 */
+	public String getQuery() {
+		return query;
+	}
+}
